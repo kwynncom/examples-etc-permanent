d5cec747bd618812d8979779dea5826ac27a1af8
kwynncom <buessresume@gmail.com>
2022-08-20 21:15:05 -0400

    JS IO web added

---
+++ ioweb/README.md
@@ -0,0 +1,4 @@
+https://kwynn.com/t/22/07/textIO/textIO.html - close to live
+
+My web version will probably fall behind for a few days.
+
---
+++ ioweb/server.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once('/opt/kwynn/kwutils.php');
+
+// sleep(random_int(0, 2));
+
+$ret['retv'] = isrv('v');
+$ret['stdtextIOTestRet'] = 'OK';
+
+kwjae($ret);
