7c94d202a8875484630bbbff973526e8449dd732
kwynncom <buessresume@gmail.com>
2022-11-25 21:03:59 -0500

    setting up for multiple rows per format of dates

--- dates/server.php
+++ dates/server.php
@@ -2,21 +2,33 @@
 
 require_once('/opt/kwynn/kwutils.php');
 
-doit();
+class dateEx {
+	
+	public function __construct() { 
+		$this->do10();
+		
+	}
+	
+	private function do20() {
+		
+	}
 
-function doit() {
-    kwas(isset($_REQUEST['formats']), 'no format sent');
-    $fs =      $_REQUEST['formats'];
-    $a = json_decode($fs, 1);
-    $ra = [];
-    
-    $o = new DateTimeImmutable();
-    
-    foreach($a as $f) {    
-	kwas(!preg_match('/^[^A-Za-z- \.-]+$/', $f), 'bad character');
-	$ra[$f] = $o->format($f);
-    }
-    
-    header('Content-Type: application/json');
-    echo(json_encode($ra));
+	private function do10() {
+		kwas(isset($_REQUEST['formats']), 'no format sent');
+		$fs =      $_REQUEST['formats'];
+		$a = json_decode($fs, 1);
+		$ra = [];
+
+		$o = new DateTimeImmutable();
+
+		foreach($a as $f) {    
+			kwas(!preg_match('/^[^A-Za-z- \.-]+$/', $f), 'bad character');
+			$ra[] = [$f => $o->format($f)];
+		}
+
+		header('Content-Type: application/json');
+		echo(json_encode($ra));
+	}
 }
+
+new dateEx();
