13f8a948d1f98e8082771828fc085aab849b8c4a
kwynncom <buessresume@gmail.com>
2022-11-25 21:12:46 -0500

    another step

--- dates/server.php
+++ dates/server.php
@@ -9,25 +9,28 @@ class dateEx {
 		
 	}
 	
-	private function do20() {
-		
-	}
-
 	private function do10() {
 		kwas(isset($_REQUEST['formats']), 'no format sent');
 		$fs =      $_REQUEST['formats'];
 		$a = json_decode($fs, 1);
 		$ra = [];
 
-		$o = new DateTimeImmutable();
-
 		foreach($a as $f) {    
 			kwas(!preg_match('/^[^A-Za-z- \.-]+$/', $f), 'bad character');
-			$ra[] = [$f => $o->format($f)];
+			$res = $this->do20($f);
+			foreach($res as $a) $ra[] = $a;
 		}
 
 		header('Content-Type: application/json');
 		echo(json_encode($ra));
+	}
+	
+	private function do20($f) {
+		$ret = [];
+		$o = new DateTime();
+		if (strpos($f, 'g')) ; 
+		$ret[] = [$f => $o->format($f)];		
+		return $ret;
 	}
 }
 
