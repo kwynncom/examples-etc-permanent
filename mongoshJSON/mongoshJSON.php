3b5ba70ff8f5b12da8a7038f2a2b985a25f1acf6
Kwynn Buess <buessresume@gmail.com>
2024-07-09 04:34:44 -0400

    more commentary 2

--- mongoshJSON/mongoshJSON.php
+++ mongoshJSON/mongoshJSON.php
@@ -5,7 +5,8 @@
 require_once('/opt/kwynn/kwutils.php'); // https://github.com/kwynncom/kwynn-php-general-utils/blob/master/kwutils.php
 $q = "print(EJSON.stringify(db.getSiblingDB('test').getCollection('my_arbitrary_collection_name').find().toArray()))";
 
-//		  dbnm ,  q, next 2 not relevant; last param accepts a raw query with true
+// The following function is the equivalent of running the query in the mongosh shell "by hand."  The function executes mongosh.
+//		  dbnm ,  q, next 2 not relevant; last param accepts a raw query with true - raw as opposed to adding the toArray and print and whatnot.
 $json = dbqcl::q('test', $q, false, '',		    true); // in https://github.com/kwynncom/kwynn-php-general-utils/blob/master/mongoDBcli.php
 $a = json_decode($json, true);
 $jsonPretty = json_encode($a, JSON_PRETTY_PRINT);
