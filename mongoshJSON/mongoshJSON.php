e79929ce2584cc11ff3349d902bf09fc108b461f
Kwynn Buess <buessresume@gmail.com>
2024-07-09 04:32:17 -0400

    a bit more commentary

--- mongoshJSON/mongoshJSON.php
+++ mongoshJSON/mongoshJSON.php
@@ -6,9 +6,7 @@ require_once('/opt/kwynn/kwutils.php'); // https://github.com/kwynncom/kwynn-php
 $q = "print(EJSON.stringify(db.getSiblingDB('test').getCollection('my_arbitrary_collection_name').find().toArray()))";
 
 //		  dbnm ,  q, next 2 not relevant; last param accepts a raw query with true
-$json = dbqcl::q('test', $q, false, '',		    true); // snapshot of current version below
-// 
-
+$json = dbqcl::q('test', $q, false, '',		    true); // in https://github.com/kwynncom/kwynn-php-general-utils/blob/master/mongoDBcli.php
 $a = json_decode($json, true);
 $jsonPretty = json_encode($a, JSON_PRETTY_PRINT);
-echo($jsonPretty);
+echo($jsonPretty); // prove it works
